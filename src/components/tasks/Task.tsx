a8dc9bc12701d9808634cebdc063ca8e89a32776
aTNa <atay.narynov@gmail.com>
2021-07-10 12:49:06 +0600

    Show previous tasks

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -6,6 +6,8 @@ import {tasksUrl} from '../../config/Urls'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomFileWidget from "../custom-widgets/file-widget/CustomFileWidget";
 import {Button} from "react-bootstrap";
+import {log} from "util";
+import {Grid, Typography} from "@material-ui/core";
 
 type RouterParams = { id: string }
 
@@ -17,21 +19,43 @@ const Task = () => {
     const [uiSchema, setUiSchema] = useState({})
     const [formResponses, setFormResponses] = useState({})
     const [complete, setComplete] = useState(false)
+    const [prevTasks, setPrevTasks] = useState<any>([])
 
     const widgets = {
         customfile: CustomFileWidget
     };
 
     useEffect(() => {
-        const getTask = () => {
+        const getTask = (stageId?: string | number, caseId?: string | number) => {
+            if (stageId && caseId) {
+                return axios.get(`${tasksUrl}?stage=${stageId}&case=${caseId}`).then((res: any) => res.data)
+            }
             return axios.get(tasksUrl + id + '/').then((res: any) => res.data)
         }
         const setData = async () => {
             let task = await getTask()
             let stage = task.stage
             console.log(task, stage)
+
             let parsed_schema = JSON.parse(stage.json_schema)
             let parsed_ui = JSON.parse(stage.ui_schema)
+            let caseId = task.case
+            let prevStages = stage.displayed_prev_stages
+            console.log(prevStages)
+            let inTasksPromise = prevStages.map((stageId: number | string) => getTask(stageId, caseId))
+            let inTasks = await Promise.all(inTasksPromise)
+                .then(res => {
+                    console.log(res)
+                    return res.map((t: any) => {
+                        return t.map((tt: any) => ({
+                            responses: tt.responses,
+                            json_schema: JSON.parse(tt.stage.json_schema),
+                            ui_schema: JSON.parse(tt.stage.ui_schema)
+                        }))
+                    }).flat()
+                })
+            console.log("inTasks", inTasks)
+            setPrevTasks(inTasks)
             setFormResponses(task.responses)
             setSchema(parsed_schema)
             setUiSchema(parsed_ui)
@@ -56,22 +80,39 @@ const Task = () => {
 
     return (
         <div style={{width: '70%', minWidth: '400px', margin: '0 auto', display: 'block', padding: 10}}>
-            <Form
-                schema={schema}
-                uiSchema={uiSchema}
-                formData={formResponses}
-                widgets={widgets}
-                disabled={complete}
-                onChange={handleChange}
-                onSubmit={handleSubmit}
-            >
-                <Button type="submit" disabled={complete}>Submit</Button>
-            </Form>
+            {prevTasks.length > 0 &&
+            <Grid>
+                <Typography variant={"h4"} align="center" style={{padding: 10}}>Previous Tasks</Typography>
+                {prevTasks.map((task: any, i: number) =>
+                    <Form
+                        key={`prev_task_${i}`}
+                        schema={task.json_schema}
+                        uiSchema={task.ui_schema}
+                        formData={task.responses}
+                        widgets={widgets}
+                        disabled={true}
+                        children={" "}
+                    />
+                )}
+            </Grid>
+            }
+            <Grid>
+                {prevTasks.length > 0 && <Typography variant={"h4"} align="center" style={{padding: 10}}>Current Task</Typography>}
+                <Form
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    formData={formResponses}
+                    widgets={widgets}
+                    disabled={complete}
+                    onChange={handleChange}
+                    onSubmit={handleSubmit}
+                >
+                    <Button type="submit" disabled={complete}>Submit</Button>
+                </Form>
+            </Grid>
         </div>
     )
 }
 
 
 export default Task
-
-

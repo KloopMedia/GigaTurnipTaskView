fb9197c8baf11a31e37a8cec056929a715056fae
aTNa <atay.narynov@gmail.com>
2021-07-10 23:16:01 +0600

    Load files

--- src/components/custom-widgets/file-widget/CustomFileWidget.tsx
+++ src/components/custom-widgets/file-widget/CustomFileWidget.tsx
@@ -1,13 +1,67 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import firebase from '../../../util/Firebase'
+import upload from "./Upload";
+import LinearProgressWithLabel from "./LinearProgressWithLabel";
 
 
 const CustomFileWidget = (props: any) => {
-    const {schema, disabled} = props;
+    const {schema, disabled, name, formContext, value} = props;
+    const {campaignId, chainId, stageId, userId, taskId} = formContext;
+    const [fileBeingUploaded, setFileBeingUploaded] = useState<any>({})
+    const [fileLinks, setFileLinks] = useState({})
+
+    let pathToFolder: any = undefined
+    if (campaignId && chainId && stageId && userId && taskId) {
+        pathToFolder = firebase
+        .storage()
+        .ref(campaignId)
+        .child(chainId)
+        .child(stageId)
+        .child(userId)
+        .child(taskId)
+    }
+
+
+    useEffect(() => {
+        console.log("value", value)
+        if (value) {
+            let newFiles: any = {}
+            let parsed = JSON.parse(value)
+            Object.keys(parsed).forEach(filename => {
+                newFiles[filename] = {url: parsed[filename], status: "complete"}
+            })
+            setFileBeingUploaded(newFiles)
+        }
+    }, [value])
+
+    const handleChange = async (event: any) => {
+        console.log("Files selected: ", [...event.target.files,])
+        upload([...event.target.files], pathToFolder, setFileBeingUploaded, setFileLinks)
+    };
+
+    useEffect(() => {
+        console.log("fileLinks", fileLinks)
+        let stringify = JSON.stringify(fileLinks)
+        props.onChange(stringify)
+    }, [fileLinks])
+
     return (
         <div>
             <label className={"form-label"}>{schema?.title}</label>
             <br/>
-            <input disabled={disabled} type="file"/>
+            <input disabled={disabled} multiple={true} type="file" onChange={handleChange}/>
+
+            {Object.keys(fileBeingUploaded).map(filename =>
+                <div key={filename}>
+                    <div style={{display: "flex"}}>
+                        <p>{filename}</p>
+                        {fileBeingUploaded[filename].status === 'complete' &&
+                        <a className="text-success" href={fileBeingUploaded[filename]?.url} style={{paddingLeft: 10}}>saved</a>}
+                    </div>
+                    {fileBeingUploaded[filename].status === 'loading' &&
+                    <LinearProgressWithLabel value={fileBeingUploaded[filename].progress}/>}
+                </div>
+            )}
         </div>
     )
 }

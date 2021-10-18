7c915c0f18432806b0abcc5ba99d8f55190c300e
aTNa <atay.narynov@gmail.com>
2021-10-18 11:25:06 +0600

    Fix required page refresh on start

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext} from "react";
 import Task from './components/tasks/Task'
 import Appbar from "./components/appbar/Appbar";
 
@@ -7,8 +7,24 @@ import SimpleAppbar from "./components/appbar/SimpleAppbar";
 import Campaigns from "./components/campaigns/Campaigns";
 import TaskMenu from "./components/tasks/TaskMenu";
 import About from "./components/campaigns/About";
+import {AuthContext} from "./util/Auth";
 
 const App = () => {
+    const {currentUser} = useContext(AuthContext)
+    if (currentUser) {
+        const token = localStorage.getItem("token");
+        currentUser.getIdToken(false).then((idToken: string) => {
+            if (token) {
+                if (idToken !== token) {
+                    localStorage.setItem("token", idToken);
+                    window.location.reload()
+                }
+            } else {
+                localStorage.setItem("token", idToken);
+                window.location.reload()
+            }
+        })
+    }
     return (
         <div>
             <Router>

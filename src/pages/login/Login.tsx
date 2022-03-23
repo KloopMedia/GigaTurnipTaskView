ae665ada4dac21292caaad8866299bc86d774855
aTNa <atay.narynov@gmail.com>
2022-03-23 11:41:03 +0600

    Add registration page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import {Button} from "@mui/material";
+import {Box, Button, Grid, Typography} from "@mui/material";
 import {useAuth} from "../../context/authentication/hooks/useAuth";
 import {useLocation, useNavigate} from "react-router-dom";
 
@@ -16,7 +16,16 @@ const Login = () => {
         login(() => navigate(from, { replace: true }))
     }
 
-    return <Button onClick={handleLogin}>Login</Button>
+    return (
+        <Box p={3}>
+            <Typography align="center" variant="h4">Регистрация</Typography>
+            <Typography variant="body1" align="center">Нажмите на кнопку (Вход или Регистрация).</Typography>
+            <Typography variant="body1" align="center">Если у вас нет аккаунта Google, то создайте его.</Typography>
+            <Box display={"flex"} justifyContent={"center"} p={2}>
+                <Button size="large" color="primary" variant="contained" fullWidth={true} onClick={handleLogin}>Регистрация</Button>
+            </Box>
+        </Box>
+    )
 }
 
 export default Login
